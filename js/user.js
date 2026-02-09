c25c8e373f93e6dae12cb78438ba1584fffce064
D0Salas <156812127+D0Salas@users.noreply.github.com>
2026-02-09 15:09:35 -0600

    Delete js directory

--- js/user.js
+++
@@ -1,9 +0,0 @@
-export class Usuario {
-  constructor(nombre = "Sin nombre", rol = "Invitado") {
-    this.nombre = nombre;
-    this.rol = rol;
-  }
-  descripcion() {
-    return `👤 Usuario: ${this.nombre} | Rol: ${this.rol}`;
-  }
-}
